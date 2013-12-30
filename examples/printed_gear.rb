ae792aec3de909f781c9f90c97ee8e8d3521bab2
Joachim Glauche <webmaster@joaz.de>
2013-12-30 16:57:33 +0100

    printed gear example; with animation

---
+++ examples/printed_gear.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/ruby1.9.3
+require "rubygems"
+require "crystalscad"
+include CrystalScad
+
+  
+g1 = PrintedGear.new(module:2.0,teeth:40,bore:5,height:4)
+g2 = PrintedGear.new(module:2.0,teeth:20,bore:5,height:4)
+
+res = g1.show.color("red").rotate(z:"$t*360")
+res += g2.show.rotate(z:"-7.5-$t*360*2").translate(x:g1.distance_to(g2))
+
+res.save("printed_gear.scad")
+
