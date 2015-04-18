37d832a2c60705093f007c79f7647b2c7d583b5a
Jennifer Glauche <webmaster@joaz.de>
2015-04-18 16:39:26 +0200

    added basic triangle solver

--- lib/crystalscad/Extras.rb
+++ lib/crystalscad/Extras.rb
@@ -1,5 +1,64 @@
 module CrystalScad::Extras
 
+
+	# This currently can solve triangels knowing 1 side and 2 angels 
+	class Triangle
+		attr_accessor :alpha, :beta, :gamma, :a, :b, :c
+		def initialize(args={})
+			@alpha = args[:alpha]
+			@beta = args[:beta]
+			@gamma = args[:gamma]
+			@a = args[:a]
+			@b = args[:b]
+			@c = args[:c]	
+			solve
+		end
+
+		def solve
+			# see if we have two angles, so we know the third one
+			if @alpha == nil and @beta != nil and @gamma != nil
+				@alpha = 180 - @beta - @gamma
+			elsif @alpha != nil and @beta == nil and @gamma != nil
+				@beta = 180 - @alpha - @gamma 
+			elsif @alpha != nil and @beta != nil and @gamma == nil			
+				@gamma = 180 - @alpha - @beta
+			end			
+
+			if @alpha != nil and @beta != nil and @gamma != nil
+				
+				if @a == nil
+					if @b != nil
+						@a = (@b / Math::sin(radians(@beta))) * Math::sin(radians(@alpha))	
+					elsif @c != nil
+						@a = (@c / Math::sin(radians(@gamma))) * Math::sin(radians(@alpha))	
+					end
+				end
+
+				if @b == nil
+					if @a != nil
+						@b = (@a / Math::sin(radians(@alpha))) * Math::sin(radians(@beta))	
+					elsif @c != nil
+						@b = (@c / Math::sin(radians(@gamma))) * Math::sin(radians(@beta))	
+					end
+				end
+
+				if @c == nil
+					if @a != nil
+						@c = (@a / Math::sin(radians(@alpha))) * Math::sin(radians(@gamma))	
+					elsif @b != nil
+						@c = (@b / Math::sin(radians(@beta))) * Math::sin(radians(@gamma))	
+					end
+				end					
+	
+			end			
+
+
+		end
+
+
+	end
+
+
 	def knurled_cube(size)
 		x = size[0]
 		y = size[1]
